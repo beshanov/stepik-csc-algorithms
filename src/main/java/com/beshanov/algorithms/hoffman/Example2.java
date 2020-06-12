852a747d47ab5b33f5cd8e631b870a29db477747
Beshanov-VV <serevarno@mail.ru>
2020-06-12 23:36:45 +0300

    Задача на декодирование строки, зашифрованной кодом Хаффмана

---
+++ src/main/java/com/beshanov/algorithms/hoffman/Example2.java
@@ -0,0 +1,48 @@
+package com.beshanov.algorithms.hoffman;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * Восстановите строку по её коду и беспрефиксному коду символов.
+ * В первой строке входного файла заданы два целых числа k и l через пробел — количество различных букв, встречающихся в строке,
+ * и размер получившейся закодированной строки, соответственно. В следующих kk строках записаны коды букв в формате "letter: code".
+ * Ни один код не является префиксом другого. Буквы могут быть перечислены в любом порядке. В качестве букв могут встречаться лишь
+ * строчные буквы латинского алфавита; каждая из этих букв встречается в строке хотя бы один раз. Наконец, в последней строке
+ * записана закодированная строка. Исходная строка и коды всех букв непусты. Заданный код таков, что закодированная строка имеет
+ * минимальный возможный размер.
+ * В первой строке выходного файла выведите строку s. Она должна состоять из строчных букв латинского алфавита. Гарантируется, что длина правильного ответа не превосходит 10^4 символов.
+ */
+public class Example2 {
+    public static void main(String[] args) {
+        new Example2().run();
+    }
+
+    private void run() {
+        Scanner sc = new Scanner(System.in);
+        String[] firstLine = sc.nextLine().split(" ");
+        int numberOfLetters = Integer.valueOf(firstLine[0]);
+        Map<String, String> codeMap = new HashMap<>();
+
+        for (int i = 0; i < numberOfLetters; i++) {
+            String line = sc.nextLine();
+            String[] letterAndCode = line.split(": ");
+            codeMap.put(letterAndCode[0], letterAndCode[1]);
+        }
+
+        String encodedString = sc.nextLine();
+
+        StringBuilder decodedString = new StringBuilder();
+        while (!encodedString.isEmpty()) {
+            for (Map.Entry<String, String> entry : codeMap.entrySet()) {
+                if (encodedString.startsWith(entry.getValue())) {
+                    decodedString.append(entry.getKey());
+                    encodedString = encodedString.substring(entry.getValue().length());
+                }
+            }
+        }
+
+        System.out.println(decodedString.toString());
+    }
+}
---
+++ src/main/java/com/beshanov/algorithms/hoffman/Example3.java
@@ -0,0 +1,107 @@
+package com.beshanov.algorithms.hoffman;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * То же, что и hoffman.Example2, но решение построено на основе обхода дерева, а не на словаре,
+ * что даёт лучшую ассимптотику
+ */
+public class Example3 {
+    public static void main(String[] args) {
+        new Example3().run();
+    }
+
+    private void run() {
+        Scanner sc = new Scanner(System.in);
+        String[] firstLine = sc.nextLine().split(" ");
+        int numberOfLetters = Integer.valueOf(firstLine[0]);
+        Map<String, String> codeMap = new HashMap<>();
+
+        for (int i = 0; i < numberOfLetters; i++) {
+            String line = sc.nextLine();
+            String[] letterAndCode = line.split(": ");
+            codeMap.put(letterAndCode[0], letterAndCode[1]);
+        }
+
+        String encodedString = sc.nextLine();
+
+        Element result = new Element();
+        Element tree = buildTree(result, codeMap);
+
+        String decodeString = decode(tree, encodedString);
+        System.out.println(decodeString);
+    }
+
+    private String decode(Element root, String encodedString) {
+        StringBuilder result = new StringBuilder();
+        Element currentElement = root;
+        for (String currentDigitCode : encodedString.split("")) {
+            if (currentDigitCode.equals("0")) {
+                if (currentElement.leftChild != null) {
+                    currentElement = currentElement.leftChild;
+                }
+                if (elementHasNoChild(currentElement)) {
+                    result.append(currentElement.value);
+                    currentElement = root;
+                    continue;
+                }
+            }
+            if (currentDigitCode.equals("1")) {
+                if (currentElement.rightChild != null) {
+                    currentElement = currentElement.rightChild;
+                }
+                if (elementHasNoChild(currentElement)) {
+                    result.append(currentElement.value);
+                    currentElement = root;
+                    continue;
+                }
+            }
+        }
+        return result.toString();
+    }
+
+    private boolean elementHasNoChild(Element element) {
+        return element.leftChild == null && element.rightChild == null;
+    }
+
+    private Element buildTree(Element result, Map<String, String> codeMap) {
+        for (Map.Entry<String, String> entry : codeMap.entrySet()) {
+            String letter = entry.getKey();
+            String code = entry.getValue();
+            String[] codeArray = code.split("");
+            Element currentElement = result;
+            for (int i = 0; i < codeArray.length; i++) {
+                if (codeArray[i].equals("0")) {
+                    if (currentElement.leftChild == null) {
+                        currentElement.leftChild = new Element();
+                        if (i == codeArray.length - 1) {
+                            currentElement.leftChild.value = letter;
+                        }
+                    }
+                    currentElement = currentElement.leftChild;
+                }
+                if (codeArray[i].equals("1")) {
+                    if (currentElement.rightChild == null) {
+                        currentElement.rightChild = new Element();
+                        if (i == codeArray.length - 1) {
+                            currentElement.rightChild.value = letter;
+                        }
+                    }
+                    currentElement = currentElement.rightChild;
+                }
+            }
+        }
+        return result;
+    }
+
+    public class Element {
+        int priority;
+        String value;
+        String code = "";
+        Element leftChild;
+        Element rightChild;
+    }
+}
+
